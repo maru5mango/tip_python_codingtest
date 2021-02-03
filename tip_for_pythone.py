df4daefbed3287df20dc331825c433fa3a46907e
maru5mango <63353110+maru5mango@users.noreply.github.com>
2021-02-03 16:30:37 +0900

    Update tip_for_pythone.py

--- tip_for_pythone.py
+++ tip_for_pythone.py
@@ -6,10 +6,58 @@ def solution(year, month, day):
 #### datetime.date(year, month, date) => 날짜 형식으로 변환 
 #### weekday() => 월요일은 0 / 숫자로 해당 날짜 요일 리턴
 
+new_list=list(map(list, zip(*board))) -> 행열을 바꿈
+new_list[a]=[i for i in new_list[a] if i!=0] -> 0인 값을 없애버리기
+
+enumerate(list, start=1) => 인덱스랑 해당 내용 튜플로 묶기
+
+#인덱스 에러
+ a[-1:]  -> 빈 배열도 가능
+  
+max(b) => 배열 b중 가장 큰 값 찾기
 
 #for 문을 돌면서 값을 조금씩 바꾸어야 할 때 map 이용
 target = [1, 2, 3, 4, 5]
 result = list(map(lambda x : x+1, target))
+result = list(filter(lambda x : x%2==0, target))
+
+#map, filter, lambda
+# 리스트 여러개를 묶어 결과 값을 도출 할 때 map
+a = [1, 2, 3, 4, 5]
+b = [2, 4, 6, 8, 10]
+>>> list(map(lambda x, y: x * y, a, b))
+
+# 해당 조건이 참인 것만 골라낼 때 filter
+a = [8, 3, 2, 10, 15, 7, 1, 9, 0, 11]
+>>> list(filter(lambda x: x > 5 and x < 10, a))
+
+# 리스트 a를 함수 f를 반복해서 해야하는 경우
+import functools
+a = [1, 2, 3, 4, 5]
+print(functools.reduce(lambda x, y: x * y, a))
+
+# 인덱스 에러 날 때
+arr[x]!=arr[x+1] => [arr[x]]!=arr[x+1:x+2]
+
+#파이썬에서 문자열에서 특정 문자 찾기
+s.lower() => s.count('p')
+
+#인덱스랑 리스트 요소 중 작은 것 출력
+map(min, enumerate(list, start=1))
+
+#파이썬 숫자를 문자열로
+str(n)
+
+#파이썬 문자열 배열을 숫자 배열로
+list(map(int,s))
+
+#파이썬 배열 뒤집어 출력, reversed()도 있음
+list
+list.reverse()
+print(list)
+
+#큰 순서대로 정렬
+list.sort(reverse=True) 
 
 import math
 math.ceil() => 올림
