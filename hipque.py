0d2257c67d196eab4c8f1a5e39e05251b5daa0d0
maru5mango <63353110+maru5mango@users.noreply.github.com>
2021-02-17 15:21:01 +0900

    Create hipque.py

---
+++ hipque.py
@@ -0,0 +1,28 @@
+import heapq
+
+#힙 추가하기
+heap=[]
+heapq.heappush(heap, 1)
+
+#힙 삭제하기
+heapq.heappop(heap) -> 최솟값 pop
+
+#힙 최솟값 보기
+heap[0]
+
+#리스트를 힙으로
+tmp = [7, 5, 8, 3]
+heapq.heapify(tmp)
+
+
+#우선 순위가 포함된 힙(튜플 사용하기)
+import heapq
+
+nums = [4, 1, 7, 3, 8, 5]
+heap = []
+
+for num in nums:
+  heapq.heappush(heap, (-num, num))  # (우선 순위, 값)
+
+while heap:
+  print(heapq.heappop(heap)[1])  # index 1
