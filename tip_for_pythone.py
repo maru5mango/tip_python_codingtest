97639702a41092de9acd7c191fa911ea4af3c251
maru5mango <63353110+maru5mango@users.noreply.github.com>
2021-02-20 11:46:26 +0900

    Update tip_for_pythone.py

--- tip_for_pythone.py
+++ tip_for_pythone.py
@@ -88,6 +88,11 @@ import math
 math.ceil() => 올림
 math.floor() => 내림
 round(num) => 반올림 ( 단 5일 경우에는 앞자리가 짝수면 내림, 홀수면 올림)
+from math import gcd
+#최대 공약수
+a=gcd(a,b)
+#최소 공배수
+a=a*b//gcd(a,b)
 
 #문자열 길이 = len(s)
 #문자열 자르기 => s[:2], s[-1:] 등 배열로 취급
